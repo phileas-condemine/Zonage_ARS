4dd1c14d17dc26bcc049a7c1172d42fa1d61a026
Philéas Condemine <phileas.condemine@sante.gouv.fr>
2021-03-11 13:38:19 +0100

    finding the path to binaries at last ?

--- R/install_deps.R
+++ R/install_deps.R
@@ -1,3 +1,6 @@
 source('https://docs.rstudio.com/rspm/admin/check-user-agent.R')
 options(HTTPUserAgent = sprintf('R/%s R (%s)', getRversion(), paste(getRversion(), R.version$platform, R.version$arch, R.version$os)))
-remotes::install_deps(dependencies = TRUE,repos='https://packagemanager.rstudio.com/all/latest')
+# https://packagemanager.rstudio.com/client/#/repos/1/overview
+  options(repos = c(REPO_NAME = "https://packagemanager.rstudio.com/all/__linux__/focal/latest"))
+# remotes::install_deps(dependencies = TRUE)
+remotes::install_deps(dependencies = TRUE,repos='https://packagemanager.rstudio.com/all/__linux__/focal/latest')
