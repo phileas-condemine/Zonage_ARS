8ad87046952f91666737dba9c01b63fd4d1e380b
Philéas Condemine <phileas.condemine@sante.gouv.fr>
2021-03-18 11:40:33 +0100

    more logs when installing deps

--- install_deps.R
+++ install_deps.R
@@ -3,6 +3,9 @@ source('https://docs.rstudio.com/rspm/admin/check-user-agent.R')
 # https://packagemanager.rstudio.com/client/#/repos/1/overview
 # options(repos = c(REPO_NAME = "https://packagemanager.rstudio.com/all/__linux__/focal/latest"))
 # remotes::install_deps(dependencies = TRUE)
+print("install remotes pkg")
 install.packages('remotes',repos='https://packagemanager.rstudio.com/all/__linux__/focal/latest')
+print("install all needed packages")
 remotes::install_deps(dependencies = TRUE,repos='https://packagemanager.rstudio.com/all/__linux__/focal/latest')
+print("done !")
 
